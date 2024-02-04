44f52ae4da58995bb35aaa4a6b62becb6c855f08
trumpetplayer2 <trumpetplayer2@outlook.com>
2024-02-04 15:56:38 -0500

    Slime Entity

---
+++ src/main/java/me/trumpetplayer2/Rebirth/PossesedEntity/GenericSlimeEntity.java
@@ -0,0 +1,71 @@
+package me.trumpetplayer2.Rebirth.PossesedEntity;
+
+import java.io.File;
+import java.util.Map;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.EntityType;
+
+import me.libraryaddict.disguise.disguisetypes.Disguise;
+import me.libraryaddict.disguise.disguisetypes.DisguiseType;
+import me.libraryaddict.disguise.disguisetypes.MobDisguise;
+import me.libraryaddict.disguise.disguisetypes.watchers.SlimeWatcher;
+
+public class GenericSlimeEntity extends GenericPossessedEntity {
+
+    int size = 1;
+    
+    public GenericSlimeEntity(EntityType ent, double health, int slimeSize) {
+        super(ent, health);
+        if(slimeSize < 1) {slimeSize = 1;}
+        size = slimeSize;
+    }
+    
+    public int getSlimeSize() {
+        return size;
+    }
+    
+    public void setSlimeSize(int slimeSize) {
+        if(slimeSize < 1) {slimeSize = 1;}
+        size = slimeSize;
+    }
+
+    @Override
+    public void save(String dataPath, FileConfiguration dataConfig, File dataFile) {
+        Map<String, Object> configValues = dataConfig.getConfigurationSection(dataPath).getValues(false);
+        for (Map.Entry<String, Object> entry : configValues.entrySet()) {
+            dataConfig.getConfigurationSection(dataPath).set(entry.getKey(), null);
+        }
+        dataPath += ".";
+        if(dataConfig.getConfigurationSection(dataPath) == null) {
+            dataConfig.createSection(dataPath);
+            }
+        dataConfig.getConfigurationSection(dataPath).set("EntityType", super.getEntityType().toString());
+        dataConfig.getConfigurationSection(dataPath).set("Size", size);
+        
+    }
+
+    @Override
+    public Disguise getDisguise() {
+        MobDisguise disguise;
+        disguise = new MobDisguise(DisguiseType.getType(super.getEntityType()));
+        disguise.setViewSelfDisguise(false);
+        
+        SlimeWatcher slime = (SlimeWatcher) disguise.getWatcher();
+        slime.setSize(size);
+        
+        disguise.setWatcher(slime);
+        
+        return disguise;
+    }
+    
+    @Override
+    public void load(String dataPath, FileConfiguration dataConfig, File dataFile) {
+        //While super.load currently doesnt do anything, run it in case it changes later
+        super.load(dataPath, dataConfig, dataFile);
+        //Load the size
+        if(dataConfig.getString(dataPath + ".Size") != null) {
+            size = dataConfig.getInt(dataPath + ".Size");
+        }
+    }
+}
--- src/main/java/me/trumpetplayer2/Rebirth/PossesedEntity/PossessedEntityList.java
+++ src/main/java/me/trumpetplayer2/Rebirth/PossesedEntity/PossessedEntityList.java
@@ -14,98 +14,145 @@ public class PossessedEntityList {
         LivingEntity temp = (LivingEntity) Bukkit.getWorlds().get(0).spawn(Bukkit.getWorlds().get(0).getSpawnLocation(), e.getEntityClass());
         maxHealth = temp.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue();
         temp.remove();
+        //Anything with Saddles, remove to prevent confusion
         switch(e) {
         case AXOLOTL:
+            //Axolotl Colors, special class
             break;
         case BEE:
+            //Generic Baby
             return new GenericBabyableEntity(e, maxHealth);
         case CAT:
+            //Cat colors - Special Class
             break;
         case CHICKEN:
+            //Generic Baby
             return new GenericBabyableEntity(e, maxHealth);
         case COD:
+            //Generic Aquatic
         	return new GenericAquaticEntity(e, maxHealth);
         case COW:
+            //Generic Baby
         	return new GenericBabyableEntity(e, maxHealth);
         case CREEPER:
+            //Charged - Special class
             break;
         case DROWNED:
+            //Generic Baby Aquatic
         	break;
         case DOLPHIN:
+            //Special Case - Dolphins Grace
             break;
         case DONKEY:
-            break;
+            //Generic Baby
+            return new GenericBabyableEntity(e, maxHealth);
         case FOX:
+            //Special Case - Snow foxes
             break;
         case FROG:
+            //Special Case - Multiple colors
             break;
         case GLOW_SQUID:
+            //Generic Aquatic
             return new GenericAquaticEntity(e, maxHealth);
         case GOAT:
+            //Generic Baby
         	return new GenericBabyableEntity(e, maxHealth);
         case HOGLIN:
+            //Generic Baby
         	return new GenericBabyableEntity(e, maxHealth);
         case HORSE:
+            //Several color horses
             break;
         case HUSK:
-        	break;
+            //Generic Baby
+            return new GenericBabyableEntity(e, maxHealth);
         case LLAMA:
+            //Dif colors of llama
             break;
         case MAGMA_CUBE:
+            //Generic Slime
             break;
         case MULE:
-            break;
+            //Generic Baby
+            return new GenericBabyableEntity(e, maxHealth);
         case MUSHROOM_COW:
+            //Brown/Red
             break;
         case PANDA:
+            //Different colors
             break;
         case PARROT:
+            //Dif colors
             break;
         case PIG:
+            //Generic Baby
         	return new GenericBabyableEntity(e, maxHealth);
         case PIGLIN:
+            //Generic Baby
         	return new GenericBabyableEntity(e, maxHealth);
         case PLAYER:
+            //Special case, planned implementation
             break;
         case POLAR_BEAR:
+            //Generic Baby
         	return new GenericBabyableEntity(e, maxHealth);
         case RABBIT:
+            //Dif Colors
             break;
         case SALMON:
+            //Generic Aquatic, this isn't bedrock
         	return new GenericAquaticEntity(e, maxHealth);
         case SHEEP:
+            //Dif Colors
             break;
         case SHULKER:
+            //Dif Colors
             break;
         case SLIME:
+            //Generic Slime
             break;
         case SNIFFER:
+            //Generic Babyable
         	return new GenericBabyableEntity(e, maxHealth);
         case SNOWMAN:
+            //Pumpkin
             break;
         case SQUID:
+            //Generic Aquatic
         	return new GenericAquaticEntity(e, maxHealth);
         case STRIDER:
-            break;
+            //Generic Baby
+            return new GenericBabyableEntity(e, maxHealth);
         case TADPOLE:
+            //Generic Aquatic
         	return new GenericAquaticEntity(e, maxHealth);
         case TRADER_LLAMA:
+            //Dif Colors
             break;
         case TROPICAL_FISH:
+            //Dif Colors
             break;
         case TURTLE:
+            //Special Case - Breath both air and water
             break;
         case VILLAGER:
+            //Special case - see player
             break;
         case WOLF:
+            //Collar Colors
             break;
         case ZOGLIN:
+            //Generic Baby
         	return new GenericBabyableEntity(e, maxHealth);
         case ZOMBIE:
+            //Generic Baby. Can't swim
         	return new GenericBabyableEntity(e, maxHealth);
         case ZOMBIE_VILLAGER:
+            //Generic Baby
             break;
         case ZOMBIFIED_PIGLIN:
+            //Generic Baby
         	return new GenericBabyableEntity(e, maxHealth);
         default:
             return new GenericPossessedEntity(e, maxHealth);
